ffbb37090944ec96915f120e4e31455233eba9c0
InhabitancyCocoon <2923719587@qq.com>
2023-11-27 21:34:30 +0800

    task1_2 revise

--- minitorch/scalar.py
+++ minitorch/scalar.py
@@ -107,7 +107,7 @@ class Scalar:
         return EQ.apply(self, b)
 
     def __sub__(self, b: ScalarLike) -> Scalar:
-        return Neg.apply(Add.apply(Neg.apply(self), b))
+        return Add.apply(self, -b)
 
     def __neg__(self) -> Scalar:
         return Neg.apply(self)
